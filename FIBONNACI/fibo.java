65699ac212c22d77b3a632d0ab3d9987b915db67
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-06 09:31:41 +0000

    FIBONNACI

---
+++ FIBONNACI/fibo.java
@@ -0,0 +1,23 @@
+public class fibo{
+
+    public static void main(String[] args) {
+        
+
+        int n = 5;
+
+        int a = 0 , b=1 ;
+
+
+        for(int i = 0 ; i< n ; i++){
+
+            System.out.println(a + "");
+            int c = a +b ;
+            a=b;
+            b=c;
+        }
+
+
+
+
+    }
+}
