c06ed192fe3128976781d517ccc989857fdee129
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-09 06:03:06 +0000

    ODDEVEN

---
+++ ODDEVEN/oddeven.java
@@ -0,0 +1,13 @@
+public class oddeven {
+
+    public static void main(String[] args) {
+        
+        int number = 7; // You can change this number to test
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
