9ea44096e9e291befb895db3ad42376e417f06dc
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-09 05:24:01 +0000

    SWAP

---
+++ SWAP/xor.java
@@ -0,0 +1,14 @@
+public class xor {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 10;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
