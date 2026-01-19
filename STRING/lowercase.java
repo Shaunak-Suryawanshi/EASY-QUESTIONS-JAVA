d27b4502d982f249040ee0ce5102541f70821a97
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-19 05:59:59 +0000

    STRING

---
+++ STRING/lowercase.java
@@ -0,0 +1,15 @@
+
+
+public class lowercase {
+
+    public static void main(String[] args) {
+        
+        String str = "HELLO WORLD";
+
+        String lowerStr = str.toLowerCase();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Lowercase String: " + lowerStr);
+    }
+    
+}
