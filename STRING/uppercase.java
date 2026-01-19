b9bea578e955885ce2975090f7964d8e8e7692b2
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-19 06:00:25 +0000

    STRING

---
+++ STRING/uppercase.java
@@ -0,0 +1,14 @@
+
+public class uppercase {
+
+    public static void main(String[] args) {
+        
+        String str = "hello world";
+
+        String upperStr = str.toUpperCase();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Uppercase String: " + upperStr);
+    }
+    
+}
