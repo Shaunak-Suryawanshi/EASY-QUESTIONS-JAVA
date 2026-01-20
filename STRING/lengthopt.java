0e7864084dedf412d589540a293dc47d32c2bb3e
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-20 02:32:44 +0000

    STRING

---
+++ STRING/lengthopt.java
@@ -0,0 +1,14 @@
+public class lengthopt {
+
+    public static void main(String[] args) {
+        
+        String str ="Hello;";
+
+        for(int i=1 ; i<=str.length(); i++) {
+            System.out.println(i);
+        }
+
+
+    }
+    
+}
