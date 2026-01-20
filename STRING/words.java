640703847df6435250684ad4a1eb4194b7afc98e
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-20 02:40:54 +0000

    STRING

---
+++ STRING/words.java
@@ -0,0 +1,15 @@
+public class words {
+
+    public static void main(String[] args) {
+        
+        String str = "HELLO THIS IS SHAUNAK" ;
+
+        String [] words = str.split(" ");
+
+        System.out.println("Number of words: " + words.length);
+
+        
+    }
+    
+    
+}
