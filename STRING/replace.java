7d71fe2368325617b8526d9db75d58dc1898dd14
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-20 02:57:40 +0000

    STRING

---
+++ STRING/replace.java
@@ -0,0 +1,11 @@
+public class replace {
+
+    public static void main(String[] args) {
+        
+        String str = "HELLO";
+
+        String newStr = str.replace('L', 'l');
+        System.out.println(newStr);
+    }
+    
+}
