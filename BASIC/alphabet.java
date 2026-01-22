f880ea1f7d6938a0422e18b0c749fe98cc63eb78
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-22 17:18:48 +0000

    BASIC

---
+++ BASIC/alphabet.java
@@ -0,0 +1,16 @@
+
+public class alphabet {
+
+    public static void main(String[] args) {
+        
+        char ch = 'A';
+
+        if((ch >= 'A' && ch <='Z') || (ch >='a' && ch <= 'z')){
+
+            System.out.println(ch + " is an alphabet.");
+        } else {
+            System.out.println(ch + " is not an alphabet.");
+        }
+    }
+    
+}
