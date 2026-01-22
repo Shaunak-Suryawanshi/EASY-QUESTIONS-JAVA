54cde182c7c44ea845cdd633faffdd0d2eca5093
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-22 17:38:34 +0000

    MATH

---
+++ MATH/natural.java
@@ -0,0 +1,13 @@
+public class natural {
+
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+    }
+    
+}
