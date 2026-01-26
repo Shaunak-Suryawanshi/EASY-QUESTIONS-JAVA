fa317fb8aad7bd204d71871d615e25dc369d1eab
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-26 17:24:39 +0000

    ARRAY

---
+++ ARRAY/reversearray.java
@@ -0,0 +1,28 @@
+public class reversearray {
+
+    public static void main(String [] args){
+
+        int [] arr ={1,2,3,4,5};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp ;
+
+            start++;
+            end--;
+
+        }
+        for(int x : arr){
+            System.out.println(x);
+
+        }
+
+
+    }
+    
+}
