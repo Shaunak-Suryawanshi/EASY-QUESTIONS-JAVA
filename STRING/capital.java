b425d8706d871a6740e730c390e124dd2e12dbac
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-27 17:32:18 +0000

    STRING

---
+++ STRING/capital.java
@@ -0,0 +1,10 @@
+public class capital {
+    
+    public static void main(String[] args) {
+        
+        String str ="hello";
+
+        String capital = str.substring(0,1).toUpperCase() + str.substring(1);
+        System.out.println(capital);
+    }
+}
