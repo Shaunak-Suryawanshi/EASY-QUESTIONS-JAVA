395979a5fdd9635593d25f07a6ca605b3da76764
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-01-30 05:21:33 +0000

    STRING

---
+++ STRING/countchar.java
@@ -0,0 +1,17 @@
+public class countchar{
+
+    public static void main(String[] args) {
+        
+        String str = "hello world";
+
+        int count = 0;
+
+        for(int i=0 ; i<str.length() ; i++){
+            if(str.charAt(i)!=' '){
+                count++;
+            }
+
+        }
+        System.out.println("Number of characters (excluding spaces): " + count);
+    }
+}
