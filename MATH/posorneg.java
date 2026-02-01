26cd934300c1fb298f47c9a4bc9d417190b14896
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-01 17:43:27 +0000

    MATH

---
+++ MATH/posorneg.java
@@ -0,0 +1,17 @@
+public class posorneg {
+    
+    public static void main(String[] args) {
+        
+        int num =10 ;
+
+        if (num < 0 ){
+
+            System.out.println("negative");
+        }
+
+        else{
+
+            System.out.println("positive");
+        }
+    }
+}
