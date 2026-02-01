191b402956f20a60a832fd9a0ae669205f444685
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-01 17:46:38 +0000

    MATH

---
+++ MATH/sumof2.java
@@ -0,0 +1,12 @@
+public class sumof2 {
+    
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 16;
+
+        int total = a +b;
+
+        System.out.println(total);
+    }
+}
