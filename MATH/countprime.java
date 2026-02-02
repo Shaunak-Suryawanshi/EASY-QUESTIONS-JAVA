d893d729ad40bb36b2f51b87b70ec07c678b31a9
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-02 02:56:50 +0000

    MATH

---
+++ MATH/countprime.java
@@ -0,0 +1,25 @@
+public class countprime {
+    
+    public static void main(String[] args) {
+        
+        int n = 20;
+        int count = 0;
+
+        for(int i=2 ; i<=n ; i++){
+            boolean isPrime = true;
+        
+
+        for(int j=2 ; j<=i/2 ; j++){
+
+            if(i % j ==0){
+                isPrime = false;
+                break;
+            } }
+            if(isPrime){
+                count ++;
+            }
+        }
+    
+    System.out.println(count);
+}
+}
