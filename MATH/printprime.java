1c02e0c61db37a36ba13b8ca4bcd8be63223c441
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-02 03:02:58 +0000

    MATH

---
+++ MATH/printprime.java
@@ -0,0 +1,24 @@
+public class printprime {
+    
+    public static void main(String[] args) {
+        
+        int n = 20;
+
+        for(int i=2 ; i<=n ;i++){
+            boolean isPrime = true;
+        
+
+        for(int j=2 ; j<=i/2 ; j++){
+
+            if(i % j==0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            System.err.println(i);
+        }
+    }
+    }
+}
