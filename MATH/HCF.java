3a956698dd9a6c55a4956b3a412c81f4f2d7d557
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-03 02:29:18 +0000

    MATH

---
+++ MATH/HCF.java
@@ -0,0 +1,25 @@
+public class HCF {
+
+    public static int gcd(int a , int b){
+
+        while(b!=0){
+            int rem = a % b;
+            a=b;
+            b=rem;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        
+        int a = 36;
+        int b = 60;
+
+        int result = gcd(a,b);
+
+        System.out.println("GCD of " + a + " and " + b + " is: " + result);
+
+
+    }
+    
+}
