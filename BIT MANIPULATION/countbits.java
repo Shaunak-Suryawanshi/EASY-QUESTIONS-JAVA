2374facefd787a767518443691a1c6aff9fdc2f2
Shaunak Suryawanshi <161214557+Shaunak-Suryawanshi@users.noreply.github.com>
2026-02-03 03:01:51 +0000

    BIT MANI

---
+++ BIT MANIPULATION/countbits.java
@@ -0,0 +1,17 @@
+
+public class countbits {
+
+    public static void main(String[] args) {
+        
+        int n = 13;
+        int count =0;
+
+        while(n > 0){
+            n = n &(n-1);
+            count++;
+        }
+
+        System.out.println(count);
+    }
+    
+}
